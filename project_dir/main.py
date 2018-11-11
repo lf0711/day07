b7c2e3e0804b8a6a0564a9cc3485e03b03228927
luofang <3346338990@qq.com>
2018-11-12 00:36:47 +0800

    多爬虫实现之二 ---爬虫实现多个解析函数

--- project_dir/main.py
+++ project_dir/main.py
@@ -1,7 +1,10 @@
 from scrapy_plus.core.engine import Engine
-from spiders import BaiduSpider
+from spiders.spiders import BaiduSpider
+from spiders.douban import DoubanSpider
+
 
 if __name__ == '__main__':
     spider = BaiduSpider()
+    spider = DoubanSpider()
     engine = Engine(spider)
     engine.start()
---
+++ project_dir/spiders/douban.py
@@ -0,0 +1,32 @@
+from scrapy_plus.core.spider import Spider
+from scrapy_plus.http1.request import Request
+from scrapy_plus.item import Item
+
+
+class DoubanSpider(Spider):
+
+    def start_request(self):
+        """
+        :return: 够在初始化对象并返回
+        """
+        start_urls1 = 'https://movie.douban.com/top250?start={}&filter='
+        for a in range(0, 225, 25):
+            start_urls = start_urls1.format(a)
+            yield Request(start_urls)
+
+    def parse(self, response):
+        """
+        解析响应对象
+        :param response:
+        :return:
+        """
+        a_s = response.xpath('//div[@class="hd"]/a')
+        for a in a_s:
+            data = {}
+            data['movie_name'] = a.xpath('./span[1]/text()')[0]
+            data['movie_url'] = a.xpath('./@href')[0]
+            print(data)
+            # yield Item(data)
+        return Item(response.url)
+
+
