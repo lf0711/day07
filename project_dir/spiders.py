919efd19a9cb76a6694624a58b65693fa645eefa
luofang <3346338990@qq.com>
2018-11-11 21:20:04 +0800

    多爬虫实现之----多请求实现

--- project_dir/spiders.py
+++ project_dir/spiders.py
@@ -5,6 +5,7 @@ from scrapy_plus.core.spider import Spider
 class BaiduSpider(Spider):
     start_urls = [
         'http://www.baidu.com',
-        'http://www.hao123.com'
+        'http://www.hao123.com',
+        'https://www.cnblogs.com'
     ]
 
