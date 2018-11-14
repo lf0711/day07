e3506989ba370781d4f4dea5a9476085830bba4b
luofang <3346338990@qq.com>
2018-11-14 14:56:47 +0800

    实现动态加载模块

--- project_dir/main.py
+++ project_dir/main.py
@@ -1,27 +1,27 @@
 from scrapy_plus.core.engine import Engine
-from spiders.baidu import BaiduSpider
-from spiders.douban import DoubanSpider
-from spiders.pipelines import BaiduPipline
-from spiders.pipelines import DoubanPipline
-from middlewares.spider_middlewares import TestSpiderMiddleware1,TestSpiderMiddleware2
-from middlewares.downloader_middlewares import TestDownloaderMiddleware1,TestDownloaderMiddleware2
+# from spiders.baidu import BaiduSpider
+# from spiders.douban import DoubanSpider
+# from spiders.pipelines import BaiduPipline
+# from spiders.pipelines import DoubanPipline
+# from middlewares.spider_middlewares import TestSpiderMiddleware1,TestSpiderMiddleware2
+# from middlewares.downloader_middlewares import TestDownloaderMiddleware1,TestDownloaderMiddleware2
 
 
 
 if __name__ == '__main__':
-    baidu_spider = BaiduSpider()
-    douban_spider = DoubanSpider()
+    # baidu_spider = BaiduSpider()
+    # douban_spider = DoubanSpider()
+    #
+    # spiders = {
+    #     BaiduSpider.name:baidu_spider,
+    #     DoubanSpider.name:douban_spider
+    # }
+    # pipelines = [
+    #     BaiduPipline(),
+    #     DoubanPipline()
+    # ]
+    # spider_mids = [TestSpiderMiddleware1(),TestSpiderMiddleware2()]
+    # downloader_mids = [TestDownloaderMiddleware1(),TestDownloaderMiddleware2()]
 
-    spiders = {
-        BaiduSpider.name:baidu_spider,
-        DoubanSpider.name:douban_spider
-    }
-    pipelines = [
-        BaiduPipline(),
-        DoubanPipline()
-    ]
-    spider_mids = [TestSpiderMiddleware1(),TestSpiderMiddleware2()]
-    downloader_mids = [TestDownloaderMiddleware1(),TestDownloaderMiddleware2()]
-
-    engine = Engine(spiders,pipelines=pipelines,spider_mids=spider_mids,downloader_mids=downloader_mids)
+    engine = Engine()
     engine.start()
--- project_dir/settings.py
+++ project_dir/settings.py
@@ -1,2 +1,27 @@
 # 修改默认日志文件名称
 DEFAULT_LOG_FILENAME = '日志.log'    # 默认日志文件名称
+
+
+# 配置要启动的爬虫
+SPIDERS = [
+    'spiders.baidu.BaiduSpider',
+    'spiders.douban.DoubanSpider'
+]
+
+# 配置要启动管道
+PIPELINES = [
+    'pipelines.BaiduPipeline',
+    'pipelines.DoubanPipeline'
+]
+
+# 配置要启动下载器中间件
+# DOWNLOADER_MIDDLEWARES = [
+#     'middleware.downloader_middleware.DownloaderMiddleware1',
+#     'middleware.downloader_middleware.DownloaderMiddleware2'
+# ]
+#
+# # 配置要启动爬虫中间件
+# SPIDER_MIDDLEWARES = [
+#     'middleware.spider_middleware.SpiderMiddleware1',
+#     'middleware.spider_middleware.SpiderMiddleware2'
+# ]
